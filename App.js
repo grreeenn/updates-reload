24d2680be45474d548a9b38a7d64c52917e6619e
Brent Vatne <brentvatne@gmail.com>
2020-10-07 09:54:19 -0700

    updates reload

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, I18nManager, StyleSheet, Text, View } from "react-native";
+import * as Updates from "expo-updates";
+
+async function toggleRTL() {
+  await I18nManager.forceRTL(I18nManager.isRTL ? false : true);
+  await Updates.reloadAsync();
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>{new Date().toString()}</Text>
+      <Text>{I18nManager.isRTL ? "RTL" : "LTR"}</Text>
+      <View style={{ marginVertical: 5 }} />
+      <Button title="Reload app" onPress={() => Updates.reloadAsync()} />
+      <View style={{ marginVertical: 5 }} />
+      <Button title="Toggle RTL" onPress={() => toggleRTL()} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
